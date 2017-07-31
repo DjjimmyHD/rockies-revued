e1a2821e41dd3a5ecdc3e2f0ccc664f17752c128
James Schultz <ninjames101@gmail.com>
2017-07-31 11:32:30 -0600

    Vuetify working and Headers and Footers installed

---
+++ notes/notes.md
@@ -0,0 +1,11 @@
+### These are my notes for refactoring the rockies locator app
+- The purpose for this repo is to start refactoring my rockies app. My hope is that I can make the project better with vue then come back again with vuex.
+- I am going to attempt to work in complete features and work-in veautify.
+- I will also be using waffle io to tack my progress and help keep me on task.
+- Lets see how this goes. I also want to do wire framing and eventually ERD but first I will probably just stick with the API call so not database.
+- Ok laid out the first wireframe in sketch
+  - Going follow the waffle tracker and clean things up and push to github
+### 7/31/2017 Monday
+- Started out this morning getting everything laid out for the Headers and Footers
+- There are some growing pains with vuetify but it is worth using to get through this. The code is ugly but it works and I can get it refactored again.
+- Going to push this to github and then work on getting the button and the display then hopefully making API call after lunch.
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+  <div id="example-1">
+
+    <main>
+    <v-layout  row wrap>
+      <v-flex xs12>
+        <v-card  dark class="primary">
+          <v-card-text id="heading" class="headline">Rockies Locator</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    </main>
+    <v-footer id="footing" :absolute="true">
+      <v-card-text>
+      <p class="title mb-0 text-xs-center">Copyright Ninjames101 2017</p>
+      </v-card-text>
+    </v-footer>
+
   </div>
+
 </template>
+
+
+
+
 
 <script>
 export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Rockies landing',
     };
   },
 };
@@ -33,21 +38,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+#example-1 {
+   text-align: center;
+ }
+#heading {
+  color: #fff;
+  background-color: #24135e;
+}
+#footing {
+  background-color: #24135e;
+  color: #fff;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
